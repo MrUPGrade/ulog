63c770cce0afd7ec62a04fc9f2787a0d13ea24a4
Jakub Czapliński <jczaplinski@pgs-soft.com>
2016-03-25 22:34:08 +0100

    version in init file.

--- ulog/__init__.py
+++ ulog/__init__.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import absolute_import
 
+__version__ = '0.3.0'
+
 from ulog.ulog import ULog
 from ulog.python_logger import PythonLogger
 from ulog.base import LogLevel, LoggerBase
